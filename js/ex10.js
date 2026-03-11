3fe35ccbaa3bd99a733c6f5fc1919c643fbb8f22
Sofia <so2003barboza@gmail.com>
2026-03-11 15:39:11 -0300

    alterações ex10

--- js/ex10.js
+++ js/ex10.js
@@ -1,8 +1,8 @@
 const Distribuidor = 0.28;
 const Imposto = 0.45;
 
-let Fabrica = parseFloat(prompt("Informe o valor de fábrica: "));
+let Fabrica = prompt("Informe o valor de fábrica: ");
 
-let ValorFinal = Fabrica * (1 + Distribuidor + Imposto);
+let ValorFinal = parseFloat(Fabrica * (1 + Distribuidor + Imposto));
 
-alert("O salário atual é de R$" + ValorFinal.toFixed(2));
+alert("O salário final é de R$" + ValorFinal.toFixed(2));
